05e142be7ba1682296e4d0409bd686e413da6276
shivanigupta22 <shivanigupta2952@gmail.com>
2023-05-09 18:03:53 +0530

    added basic media3 implementation

--- app/src/main/java/iskcon/devotees/podcast/ui/media3/Media3Activity.kt
+++ app/src/main/java/iskcon/devotees/podcast/ui/media3/Media3Activity.kt
@@ -1,11 +1,14 @@
 package iskcon.devotees.podcast.ui.media3
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
-import androidx.media3.common.MediaItem
+
+import androidx.media3.common.MimeTypes
+import androidx.media3.common.Player
 import androidx.media3.exoplayer.ExoPlayer
 import iskcon.devotees.podcast.R
 import iskcon.devotees.podcast.databinding.ActivityMedia3Binding
@@ -18,6 +21,19 @@ class Media3Activity : AppCompatActivity() {
     private var playWhenReady = true
     private var currentItem = 0
     private var playbackPosition = 0L
+
+    private val playbackStateListener = object : Player.Listener {
+        override fun onPlaybackStateChanged(playbackState: Int) {
+            val stateString: String = when (playbackState) {
+                ExoPlayer.STATE_IDLE -> "ExoPlayer.STATE_IDLE      -"
+                ExoPlayer.STATE_BUFFERING -> "ExoPlayer.STATE_BUFFERING -"
+                ExoPlayer.STATE_READY -> "ExoPlayer.STATE_READY     -"
+                ExoPlayer.STATE_ENDED -> "ExoPlayer.STATE_ENDED     -"
+                else -> "UNKNOWN_STATE             -"
+            }
+            Log.d("Playback state", "changed state to $stateString")
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,11 +56,22 @@ class Media3Activity : AppCompatActivity() {
     }
 
     private fun initializePlayer() {
-        mediaPlayer = ExoPlayer.Builder(this).build().also { exoPlayer ->
-            viewBinding.videoView.player = exoPlayer
-            val mediaItem = MediaItem.fromUri(getString(R.string.media_url_mp3))
-            exoPlayer.setMediaItem(mediaItem)
-        }
+        mediaPlayer = ExoPlayer.Builder(this)
+            .build().also { exoplayer ->
+                viewBinding.videoView.player = exoplayer
+                exoplayer.addMediaItemList(
+                    arrayListOf(
+                        createMediaItem(
+                            getString(R.string.media_url_mp3),
+                            MimeTypes.AUDIO_MP4
+                        ), createMediaItem(getString(R.string.media_url_mp3_2))
+                    )
+                )
+                exoplayer.playWhenReady = playWhenReady
+                exoplayer.seekTo(currentItem, playbackPosition)
+                exoplayer.addListener(playbackStateListener)
+                exoplayer.prepare()
+            }
     }
 
     private fun hideSystemUI() {
@@ -58,10 +85,11 @@ class Media3Activity : AppCompatActivity() {
 
     private fun releasePlayer() {
         mediaPlayer?.let { exoPlayer ->
-//            playbackPosition = exoPlayer.currentPosition
-//            currentItem = exoPlayer.currentMediaItemIndex
-//            playWhenReady = exoPlayer.playWhenReady
+            playbackPosition = exoPlayer.currentPosition
+            currentItem = exoPlayer.currentMediaItemIndex
+            playWhenReady = exoPlayer.playWhenReady
             exoPlayer.release()
+            exoPlayer.removeListener(playbackStateListener)
         }
         mediaPlayer = null
     }
